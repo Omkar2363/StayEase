625af9b4aabf9a26f02dd0436ef0b2942507b263
Omkar2363 <Om.236300@gmail.com>
2024-06-10 13:23:40 +0530

    Added the Logging to the HotelServiceImpl Class

--- src/main/java/com/org/StayEase/services/Impl/HotelServiceImpl.java
+++ src/main/java/com/org/StayEase/services/Impl/HotelServiceImpl.java
@@ -5,53 +5,74 @@ import com.org.StayEase.exceptions.HotelAlreadyExistException;
 import com.org.StayEase.exceptions.HotelNotFoundException;
 import com.org.StayEase.repositories.HotelRepository;
 import com.org.StayEase.services.HotelService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-
 @Service
 public class HotelServiceImpl implements HotelService {
+
+    private static final Logger logger = LoggerFactory.getLogger(HotelServiceImpl.class);
 
     @Autowired
     private HotelRepository hotelRepository;
 
-
     @Override
     public Hotel createHotel(Hotel hotel) {
+        logger.info("Creating hotel: {}", hotel.getName());
         if (hotelRepository.findByName(hotel.getName()).isPresent()) {
+            logger.error("Hotel with name '{}' already exists", hotel.getName());
             throw new HotelAlreadyExistException("Hotel with given hotel name already exist. Use some other name...!!!");
         }
-        return hotelRepository.save(hotel);
+        Hotel createdHotel = hotelRepository.save(hotel);
+        logger.info("Hotel created with ID: {}", createdHotel.getId());
+        return createdHotel;
     }
 
     @Override
     public Hotel getHotelById(Long hotelId) {
+        logger.info("Getting hotel by ID: {}", hotelId);
         return hotelRepository.findById(hotelId)
-                .orElseThrow(()->new HotelNotFoundException("Hotel with hotelId "+hotelId+" is not available"));
+                .orElseThrow(() -> {
+                    logger.error("Hotel with ID '{}' not found", hotelId);
+                    return new HotelNotFoundException("Hotel with hotelId " + hotelId + " is not available");
+                });
     }
 
     @Override
     public List<Hotel> getAllHotels() {
+        logger.info("Getting all hotels");
         return hotelRepository.findAll();
     }
 
     @Override
     public Hotel updateHotel(Long hotelId, Hotel updateHotel) {
+        logger.info("Updating hotel with ID: {}", hotelId);
         Hotel hotel = hotelRepository.findById(hotelId)
-                .orElseThrow(()->new HotelNotFoundException("Hotel with hotelId "+hotelId+" is not available"));
+                .orElseThrow(() -> {
+                    logger.error("Hotel with ID '{}' not found", hotelId);
+                    return new HotelNotFoundException("Hotel with hotelId " + hotelId + " is not available");
+                });
 
         updateHotel.setId(hotel.getId());
         Hotel updatedHotel = hotelRepository.save(updateHotel);
+        logger.info("Hotel with ID '{}' updated", updatedHotel.getId());
         return updatedHotel;
     }
 
     @Override
     public void deleteHotel(Long hotelId) {
+        logger.info("Deleting hotel with ID: {}", hotelId);
         Hotel hotel = hotelRepository.findById(hotelId)
-                .orElseThrow(()->new HotelNotFoundException("Hotel with hotelId "+hotelId+" is not available"));
+                .orElseThrow(() -> {
+                    logger.error("Hotel with ID '{}' not found", hotelId);
+                    return new HotelNotFoundException("Hotel with hotelId " + hotelId + " is not available");
+                });
 
         hotelRepository.delete(hotel);
+        logger.info("Hotel with ID '{}' deleted", hotelId);
     }
 }
